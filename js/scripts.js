d3afa653e9b04d4491e6e92309d1fa529a037e87
Sierra Kruisselbrink <sierra.kruisselbrink@uleth.ca>
2024-11-25 16:14:44 -0700

    finished project

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
 let currentImageIndex = 1;
 const maxImages = 5;
 
-document.getElementById('waterButton').addEventListener('click', function () {
-    if (currentImageIndex < maxImages) {
+function updateImage() {
+    if (currentImageIndex < maxImages - 1) {
         currentImageIndex++;
         document.getElementById('mainImage').src = `images/transparent/${currentImageIndex}.png`;
     } else {
-        currentImageIndex = 1;
+        currentImageIndex++;
         document.getElementById('mainImage').src = `images/transparent/${currentImageIndex}.png`;
+        // hide the buttons
+        document.getElementById('waterButton').style.display = 'none';
+        document.getElementById('sunButton').style.display = 'none';
+        document.getElementById('foodButton').style.display = 'none';
+        // show the restart button
+        document.getElementById('regrowButton').style.display = 'block';
     }
-});
+}
+
+function showButtons() {
+    currentImageIndex = 1;
+    document.getElementById('mainImage').src = `images/transparent/${currentImageIndex}.png`;
+    document.getElementById('waterButton').style.display = 'block';
+    document.getElementById('sunButton').style.display = 'block';
+    document.getElementById('foodButton').style.display = 'block';
+    document.getElementById('regrowButton').style.display = 'none';
+}
+
+document.getElementById('regrowButton').style.display = 'none';
+document.getElementById('waterButton').addEventListener('click', updateImage);
+document.getElementById('sunButton').addEventListener('click', updateImage);
+document.getElementById('foodButton').addEventListener('click', updateImage);
+document.getElementById('regrowButton').addEventListener('click', showButtons);
